d8d928a1c9c68ee8ca57beeab56162bdea6500da
Santi Balaguer <santiago.balaguer@gmail.com>
2020-01-16 19:06:55 +0000

    added CV

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const test2Left = document.querySelector(".test2-left");
 const test2Right = document.querySelector(".test2-right");
 
-const tl = new TimelineLite({ paused: false });
-tl.to(".test2-left", 1.5, {
-  width: "550%",
+const tl = new TimelineLite({ paused: true });
+tl.to(".test2-right", 1.3, {
+  width: "0%",
   ease: Power2.easeOut
+})
+  .to(
+    ".test2-left",
+    1.3,
+    {
+      width: "100%",
+      ease: Power2.easeOut
+    },
+    "-=1.3"
+  )
+  .fromTo(
+    ".test-hi",
+    1.3,
+    {
+      x: 0
+    },
+    {
+      x: "50vw",
+      ease: Power2.easeOut
+    },
+    "-=1.3"
+  );
+
+window.addEventListener("click", () => {
+  tl.play();
 });
-tl.fromTo(
-  ".test-hi",
-  1.5,
-  {
-    x: 0
-  },
-  {
-    x: "50vw",
-    ease: Power2.easeOut
-  },
-  "-=1.5"
-);
